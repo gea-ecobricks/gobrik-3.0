fd434a922f44559fa42a759f7b269c7da95ff95f
Russell Maier <russ@earthen.io>
2024-07-19 13:49:11 +0800

    hta

--- en/index.php
+++ en/index.php
@@ -1,6 +1,6 @@
 <?php
 include 'lang.php';
-$version = '0.252';
+$version = '0.253';
 $page = 'index';
 include '../ecobricks_env.php';
 
@@ -25,8 +25,32 @@ https://github.com/gea-ecobricks/gobrik-3.0/tree/main/en-->
 
 
 <?php require_once ("../includes/index-inc.php");?>
-<br><br>
-<br><br>
+
+
+
+    <div class="landing-content" >
+
+
+        <div class="clouds-new2" style=" padding-top:10vh; margin-bottom: -30px;
+        padding-bottom: 10px;margin-top:-10px">
+
+            <div class="biosphere"><img src="../webp/biosphere-blank.webp" width="400" height="400" alt="biosphere"></div>
+
+            <div class="main-landing-graphic" style="width:100%;height:43%;"><img src="https://www.gobrik.com/webp/ecobrick-team-blank.webp" style="width:100%;height:43%;" alt="Unite with ecobrickers around the world"></div>
+
+
+
+
+
+
+          <!-- <div class="main-landing-graphic" style="text-align:center;background:none;">   <img src="../images/world-oceans-day-2023-700px-trans.png" style="width:80%;" alt="Build your greenest visions with ecobricks" style="text-align:center">
+
+           <img src="https://www.gobrik.com/webp/ecobrick-team-blank.webp" style="min-width: 250px;
+    min-height: 145px; width:100%;" width="250" height="112" alt="Unite">
+-->
+</div>
+
+
 <br><br>
 <p data-lang-id="000-testing">Testing one two and three</p>
 

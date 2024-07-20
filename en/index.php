2ead884e7877d33a1bdc9bdbfd4a85fdc307052d
Russell Maier <russ@earthen.io>
2024-07-20 21:47:47 +0800

    d

--- en/index.php
+++ en/index.php
@@ -1,7 +1,7 @@
 <?php
 
 include 'lang.php';
-$version = '0.345';
+$version = '0.346';
 $page = 'index';
 include '../ecobricks_env.php';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));

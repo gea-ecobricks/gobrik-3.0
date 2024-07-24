c5d6bce923eac3007d80eb945804ed83620eeeb5
Russell Maier <russ@earthen.io>
2024-07-24 12:52:29 +0800

    sep

--- en/signup_process.php
+++ en/signup_process.php
@@ -8,56 +8,6 @@ $user_id = $_GET['id'] ?? null;
 
 include '../buwana_env.php'; // This file provides the database server, user, dbname information to access the server
 
-// PART 1: Check if the user is already logged in
-if (isset($_SESSION['user_id'])) {
-    $response['error'] = 'logged_in';
-    echo json_encode($response);
-    exit();
-}
-
-$credential_type = '';
-$credential_key = '';
-$first_name = '';
-$account_status = '';
-
-if (isset($user_id)) {
-    $sql_lookup_credential = "SELECT credential_type, credential_key FROM credentials_tb WHERE user_id = ?";
-    $stmt_lookup_credential = $conn->prepare($sql_lookup_credential);
-    if ($stmt_lookup_credential) {
-        $stmt_lookup_credential->bind_param("i", $user_id);
-        $stmt_lookup_credential->execute();
-        $stmt_lookup_credential->bind_result($credential_type, $credential_key);
-        $stmt_lookup_credential->fetch();
-        $stmt_lookup_credential->close();
-    } else {
-        $response['error'] = 'db_error';
-        echo json_encode($response);
-        exit();
-    }
-
-    $sql_lookup_user = "SELECT first_name, account_status FROM users_tb WHERE user_id = ?";
-    $stmt_lookup_user = $conn->prepare($sql_lookup_user);
-    if ($stmt_lookup_user) {
-        $stmt_lookup_user->bind_param("i", $user_id);
-        $stmt_lookup_user->execute();
-        $stmt_lookup_user->bind_result($first_name, $account_status);
-        $stmt_lookup_user->fetch();
-        $stmt_lookup_user->close();
-    } else {
-        $response['error'] = 'db_error';
-        echo json_encode($response);
-        exit();
-    }
-
-    $credential_type = htmlspecialchars($credential_type);
-    $first_name = htmlspecialchars($first_name);
-
-    if ($account_status !== 'name set only') {
-        $response['error'] = 'account_status';
-        echo json_encode($response);
-        exit();
-    }
-}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($user_id)) {
     $credential_value = htmlspecialchars($_POST['credential_value']);

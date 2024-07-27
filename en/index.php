8a116085fb13da9de65029020e90f8e04d4dd61e
Russell Maier <russ@earthen.io>
2024-07-27 10:51:52 +0800

    knack update

--- en/index.php
+++ en/index.php
@@ -1,7 +1,7 @@
 <?php
 $directory = basename(dirname($_SERVER['SCRIPT_NAME']));
 $lang = $directory;
-$version = '0.36';
+$version = '0.37';
 $page = 'index';
 include '../ecobricks_env.php';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));

0c18379e68a25bae40ea4da6843b65386bc0cdb1
Russell Maier <russ@earthen.io>
2024-07-27 09:09:30 +0800

    knack update

--- en/login.php
+++ en/login.php
@@ -1,7 +1,7 @@
 <?php
 $directory = basename(dirname($_SERVER['SCRIPT_NAME']));  //grabs language directory from url
 $lang = $directory;
-$version = '0.3999';
+$version = '0.4';
 $page = 'login';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 

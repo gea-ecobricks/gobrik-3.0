0489e9c6911e0e4b214cd53494a65864bde8b266
Russell Maier <russ@earthen.io>
2024-07-28 15:40:29 +0800

    table update

--- en/dashboard.php
+++ en/dashboard.php
@@ -134,29 +134,32 @@ https://github.com/gea-ecobricks/gobrik-3.0/tree/main/en-->
                     <th data-lang-id="1106-maker">Maker</th>
                     <th data-lang-id="1107-serial">Serial</th>
                 </tr>
-                <?php foreach ($recent_ecobricks as $ecobrick) : ?>
-                    <tr>
-                        <td>
-                            <img src="https://ecobricks.org/<?php echo htmlspecialchars($ecobrick['ecobrick_thumb_photo_url']); ?>"
-                                 alt="Ecobrick Thumbnail"
-                                 onclick="ecobrickPreview('<?php echo htmlspecialchars($ecobrick['ecobrick_full_photo_url']); ?>',
-                                                         '<?php echo htmlspecialchars($ecobrick['serial_no']); ?>',
-                                                         '<?php echo htmlspecialchars($ecobrick['weight_g']); ?>g',
-                                                         '<?php echo htmlspecialchars($ecobrick['ecobricker_maker']); ?>',
-                                                         '<?php echo htmlspecialchars($ecobrick['location_full']); ?>')"
-                                 class="table-thumbnail">
-                        </td>
-                        <td><?php echo htmlspecialchars($ecobrick['weight_g']); ?>g</td>
-                        <td><?php echo htmlspecialchars($ecobrick['location_full']); ?></td>
-                        <td><?php echo htmlspecialchars($ecobrick['ecobricker_maker']); ?></td>
-                        <td><button class="serial-button"><?php
-$serial_no = htmlspecialchars($ecobrick['serial_no']);
-$wrapped_serial_no = substr($serial_no, 0, 3) . '<br>' . substr($serial_no, 3, 3);
-?>
-<a href="brik.php?serial_no=<?php echo $serial_no; ?>"><?php echo $wrapped_serial_no; ?></a>
-</td>
-                    </tr>
-                <?php endforeach; ?>
+               <?php foreach ($recent_ecobricks as $ecobrick) : ?>
+    <tr>
+        <td>
+            <img src="https://ecobricks.org/<?php echo htmlspecialchars($ecobrick['ecobrick_thumb_photo_url']); ?>"
+                 alt="Ecobrick Thumbnail"
+                 onclick="ecobrickPreview('<?php echo htmlspecialchars($ecobrick['ecobrick_full_photo_url']); ?>',
+                                         '<?php echo htmlspecialchars($ecobrick['serial_no']); ?>',
+                                         '<?php echo htmlspecialchars($ecobrick['weight_g']); ?>g',
+                                         '<?php echo htmlspecialchars($ecobrick['ecobricker_maker']); ?>',
+                                         '<?php echo htmlspecialchars($ecobrick['location_full']); ?>')"
+                 class="table-thumbnail">
+        </td>
+        <td><?php echo htmlspecialchars($ecobrick['weight_g']); ?>g</td>
+        <td><?php echo htmlspecialchars($ecobrick['location_full']); ?></td>
+        <td><?php echo htmlspecialchars($ecobrick['ecobricker_maker']); ?></td>
+        <td>
+            <button class="serial-button">
+                <?php
+                $serial_no = htmlspecialchars($ecobrick['serial_no']);
+                $wrapped_serial_no = substr($serial_no, 0, 3) . '<br>' . substr($serial_no, 3, 3);
+                ?>
+                <a href="brik.php?serial_no=<?php echo $serial_no; ?>"><?php echo $wrapped_serial_no; ?></a>
+            </button>
+        </td>
+    </tr>
+<?php endforeach; ?>
             </table>
         </div>
 

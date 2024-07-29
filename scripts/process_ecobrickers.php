50ac41857fcbf91c42b9c4abd2b43eb071fb49da
Russell Maier <russ@earthen.io>
2024-07-29 19:27:28 +0800

    and click

--- scripts/process_ecobrickers.php
+++ scripts/process_ecobrickers.php
@@ -61,9 +61,9 @@
         $conn->set_charset("utf8mb4");
 
         // SQL query to fetch the latest 10 ecobrickers
-        $query = "SELECT first_name, buwana_id, location_full_txt, date_registered, email_addr FROM ecobricker_live_tb
+        $query = "SELECT first_name, buwana_id, location_full_txt, date_registered, email_addr FROM tb_ecobrickers
                   ORDER BY date_registered DESC
-                  LIMIT 10";
+                  LIMIT 1";
 
         $result = $conn->query($query);
         ?>
@@ -102,6 +102,7 @@
     <input type="email" id="email" name="email" required>
     <button type="submit">Retrieve</button>
 </form>
+
 
 <!-- Part 4: Process and Upload Data to GoBrik Database -->
 <div id="knack-response">
@@ -171,7 +172,7 @@
                 $region_txt = $record['field_359'];
                 $city_txt = $record['field_342'];
                 $location_full_txt = $record['field_429'];
-                $household_txt = $record['field_2028'];
+                $household_ttx = $record['field_2028'];
                 $gender = $record['field_283'];
                 $personal_catalyst = $record['field_1676'];
                 $trainer_availability = $record['field_430'];
@@ -183,7 +184,7 @@
                 $household = $record['field_2038'];
 
                 // Insert the data into tb_ecobrickers
-                $sql_insert = "INSERT INTO tb_ecobrickers (maker_id, legacy_gobrik_user_id, username, first_name, last_name, full_name, user_roles, gea_status, community, email_addr, date_registered, phone_no, ecobricks_made, brk_balance, aes_balance, aes_purchased, country_txt, region_txt, city_txt, location_full_txt, household_ttx, gender, personal_catalyst, trainer_availability, pronouns, household_generation, country_per_capita_consumption, my_consumption_estimate, household_members, household, buwana_activated, gobrik_migrated, account_notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 'No', '1', 'migrated from knack gobrik on July 29th, 2024')";
+                $sql_insert = "INSERT INTO tb_ecobrickers (maker_id, legacy_gobrik_user_id, username, first_name, last_name, full_name, user_roles, gea_status, community, email_addr, date_registered, phone_no, ecobricks_made, brk_balance, aes_balance, aes_purchased, country_txt, region_txt, city_txt, location_full_txt, household_ttx, gender, personal_catalyst, trainer_availability, pronouns, household_generation, country_per_capita_consumption, my_consumption_estimate, household_members, household, buwana_activated, gobrik_migrated, account_notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0, 1, 'migrated from knack gobrik on July 29th, 2024')";
 
                 $stmt_insert = $conn->prepare($sql_insert);
                 if ($stmt_insert) {
@@ -209,7 +210,7 @@
                         $region_txt,
                         $city_txt,
                         $location_full_txt,
-                        $household_txt,
+                        $household_ttx,
                         $gender,
                         $personal_catalyst,
                         $trainer_availability,
@@ -241,5 +242,6 @@
     }
     ?>
 </div>
+
 </body>
 </html>

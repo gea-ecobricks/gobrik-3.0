1730a8812521bc80e5319ff0a12d23369e27a8b1
Russell Maier <russ@earthen.io>
2024-07-29 20:09:07 +0800

    and click

--- scripts/process_ecobrickers.php
+++ scripts/process_ecobrickers.php
@@ -187,39 +187,7 @@
                 $stmt_insert = $conn->prepare($sql_insert);
                 if ($stmt_insert) {
                     $stmt_insert->bind_param(
-                        's
-i
-s
-s
-s
-s
-s
-s
-s
-s
-s
-i
-d
-s
-s
-s
-s
-s
-s
-s
-s
-s
-s
-s
-d
-d
-d
-i
-i
-i
-i
-s
-',
+                        'sisssssssssidsssssssssssdddiiiis',
                         $record_id,
                         $legacy_gobrik_user_id,
                         $first_name,

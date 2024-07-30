362eef4c7191686354b4e99e5649de3fd4e14e10
Russell Maier <russ@earthen.io>
2024-07-30 17:30:31 +0800

    and click

--- scripts/process_ecobrickers.php
+++ scripts/process_ecobrickers.php
@@ -152,7 +152,7 @@
                     $gender = $record['field_283'] ?? '';
                     $personal_catalyst = strip_tags($record['field_1676'] ?? '');
                     $trainer_availability = $record['field_430'] ?? '';
-                    $pronouns = $record['field_552'] ?? '';
+                    $pronoun = $record['field_552'] ?? '';
                     $household_generation = $record['field_2231'] ?? 0;
                     $country_per_capita_consumption = $record['field_2106'] ?? 0;
                     $my_consumption_estimate = $record['field_2221'] ?? 0;
@@ -166,7 +166,7 @@
                     $gobrik_migrated_dt = date('Y-m-d H:i:s');
 
                     // Insert the data into tb_ecobrickers
-                    $sql_insert = "INSERT INTO tb_ecobrickers (maker_id, legacy_gobrik_user_id, first_name, last_name, full_name, user_roles, gea_status, community, email_addr, date_registered, phone_no, ecobricks_made, brk_balance, aes_balance, aes_purchased, country_txt, region_txt, city_txt, location_full_txt, household_txt, gender, personal_catalyst, trainer_availability, pronouns, household_generation, country_per_capita_consumption, my_consumption_estimate, household_members, household, buwana_activated, gobrik_migrated, account_notes, gobrik_migrated_dt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+                    $sql_insert = "INSERT INTO tb_ecobrickers (maker_id, legacy_gobrik_user_id, first_name, last_name, full_name, user_roles, gea_status, community, email_addr, date_registered, phone_no, ecobricks_made, brk_balance, aes_balance, aes_purchased, country_txt, region_txt, city_txt, location_full_txt, household_txt, gender, personal_catalyst, trainer_availability, pronoun, household_generation, country_per_capita_consumption, my_consumption_estimate, household_members, household, buwana_activated, gobrik_migrated, account_notes, gobrik_migrated_dt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
                     $stmt_insert = $conn->prepare($sql_insert);
                     if ($stmt_insert) {
@@ -195,7 +195,7 @@
                             $gender,
                             $personal_catalyst,
                             $trainer_availability,
-                            $pronouns,
+                            $pronoun,
                             $household_generation,
                             $country_per_capita_consumption,
                             $my_consumption_estimate,

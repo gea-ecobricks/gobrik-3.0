ba2e39d3300f3c1c01518edc161362ac6ba77bf9
Russell Maier <russ@earthen.io>
2024-07-30 23:01:20 +0800

    and click

--- scripts/process_ecobrickers.php
+++ scripts/process_ecobrickers.php
@@ -94,7 +94,6 @@
     <button type="submit">Start Migration</button>
 </form>
 
-
 <!-- Part 4: Process and Upload Data to GoBrik Database -->
 <div id="knack-response">
     <?php
@@ -272,6 +271,7 @@
     }
     ?>
 </div>
+
 
 </body>
 

1eabd4d554a29fbd6e285d25851a8ad687a48289
Russell Maier <russ@earthen.io>
2024-08-03 21:05:12 +0800

    and click

--- en/login.php
+++ en/login.php
@@ -1,6 +1,6 @@
 <?php
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));  //grabs language directory from url
-$version = '0.55';
+$version = '0.555';
 $page = 'login';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 

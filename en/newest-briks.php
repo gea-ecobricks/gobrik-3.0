8a27520c8b5e37f955139d0d0aa75129bb4c5fdf
Russell Maier <russ@earthen.io>
2024-08-06 09:02:56 +0800

    bottom

--- en/newest-briks.php
+++ en/newest-briks.php
@@ -1,47 +1,12 @@
 <?php
-session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Set up page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
 $version = '0.365';
-$page = 'dashboard';
+$page = 'newest-briks';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
-
-// Check if the user is logged in
-if (!isset($_SESSION['buwana_id'])) {
-    echo '<script>alert("Please login before viewing this page."); window.location.href = "login.php";</script>';
-    exit();
-}
-
-$buwana_id = $_SESSION['buwana_id'];
-
-// Include database connection
-include '../buwana_env.php'; // this file provides the database server, user, dbname information to access the server
-
-// Look up fields from users_tb using the buwana_id
-$first_name = '';
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql_lookup_user = "SELECT first_name FROM users_tb WHERE buwana_id = ?";
-$stmt_lookup_user = $conn->prepare($sql_lookup_user);
-
-if ($stmt_lookup_user) {
-    $stmt_lookup_user->bind_param("i", $buwana_id);
-    $stmt_lookup_user->execute();
-    $stmt_lookup_user->bind_result($first_name);
-    $stmt_lookup_user->fetch();
-    $stmt_lookup_user->close();
-} else {
-    die("Error preparing statement for users_tb: " . $conn->error);
-}
-
-$conn->close();
 
 // Include GoBrik database credentials
 include '../gobrik_env.php';
@@ -52,6 +17,7 @@ $conn2 = new mysqli($servername, $username, $password, $dbname);
 if ($conn2->connect_error) {
     die("Connection failed: " . $conn2->connect_error);
 }
+
 // SQL query to fetch the count of ecobricks and the sum of weight_g divided by 1000 to get kg
 $sql = "SELECT COUNT(*) as ecobrick_count, SUM(weight_g) / 1000 as total_weight FROM tb_ecobricks";
 $result = $conn2->query($sql);
@@ -71,7 +37,7 @@ $result_recent = $conn2->query($sql_recent);
 
 $recent_ecobricks = [];
 if ($result_recent->num_rows > 0) {
-    while($row = $result_recent->fetch_assoc()) {
+    while ($row = $result_recent->fetch_assoc()) {
         $recent_ecobricks[] = $row;
     }
 }
@@ -82,14 +48,11 @@ echo '<!DOCTYPE html>
 <html lang="' . $lang . '">
 <head>
 <meta charset="UTF-8">
-<title>Dashboard</title>
-'
+<title>Newest Ecobricks</title>
+';
 ?>
 
-
-
-
-<title>Dashboard | GoBrik 3.0</title>
+<title>Newest Ecobricks | GoBrik 3.0</title>
 
 <!--
 GoBrik.com site version 3.0
@@ -97,7 +60,7 @@ Developed and made open source by the Global Ecobrick Alliance
 See our git hub repository for the full code and to help out:
 https://github.com/gea-ecobricks/gobrik-3.0/tree/main/en-->
 
-<?php require_once ("../includes/dashboard-inc.php"); ?>
+<?php require_once("../includes/dashboard-inc.php"); ?>
 
 <div class="splash-content-block"></div>
 <div id="splash-bar"></div>
@@ -107,14 +70,13 @@ https://github.com/gea-ecobricks/gobrik-3.0/tree/main/en-->
 <div id="form-submission-box" style="height:fit-content;">
     <div class="form-container">
         <div style="text-align:center;width:100%;margin:auto;">
-            <h2>Welcome <?php echo htmlspecialchars($first_name); ?>!</h2>
-            <h3>You're logged into the brand new GoBrik 3.0!</h3>
+            <h2>The Latest Ecobricks</h2>
             <p>As of today, <?php echo $ecobrick_count; ?> ecobricks have been logged on GoBrik, representing over <?php echo round($total_weight); ?> kg of sequestered plastic!</p>
         </div>
-   <div style="display:flex;flex-flow:row;width:100%;justify-content:center;">
+        <div style="display:flex;flex-flow:row;width:100%;justify-content:center;">
             <button class="go-button" id="log-ecobrick-button">➕ Log an Ecobrick</button>
-            <!-- Logout Button -->
-            <button class="go-button" id="logout-button" onclick="logoutUser()">📤 Logout</button>
+            <!-- Login Button -->
+            <button class="go-button" id="login-button" onclick="loginUser()">📤 Log In</button>
         </div>
 
         <div style="text-align:center;width:100%;margin:auto;margin-top:25px;">
@@ -127,40 +89,34 @@ https://github.com/gea-ecobricks/gobrik-3.0/tree/main/en-->
                     <th data-lang-id="1106-status">Status</th>
                     <th data-lang-id="1107-serial">Serial</th>
                 </tr>
-               <?php foreach ($recent_ecobricks as $ecobrick) : ?>
-    <tr>
-<td>
-    <img src="https://ecobricks.org/<?php echo htmlspecialchars($ecobrick['ecobrick_thumb_photo_url']); ?>"
-         alt="Ecobrick Thumbnail"
-         class="table-thumbnail"
-         onclick="ecobrickPreview('<?php echo htmlspecialchars($ecobrick['ecobrick_full_photo_url']); ?>', '<?php echo htmlspecialchars($ecobrick['serial_no']); ?>', '<?php echo htmlspecialchars($ecobrick['weight_g']); ?>g', '<?php echo htmlspecialchars($ecobrick['ecobricker_maker']); ?>', '<?php echo htmlspecialchars($ecobrick['location_full']); ?>')">
-</td>
-
-        <td><?php echo htmlspecialchars($ecobrick['weight_g']); ?>g</td>
-        <td><?php echo htmlspecialchars($ecobrick['location_full']); ?></td>
-        <td><?php echo htmlspecialchars($ecobrick['status']); ?></td>
-        <td>
-            <button class="serial-button">
-                <?php $serial_no = htmlspecialchars($ecobrick['serial_no']); $wrapped_serial_no = substr($serial_no, 0, 3) . '<br>' . substr($serial_no, 3, 3);?>
-                <a href="brik.php?serial_no=<?php echo $serial_no; ?>"><?php echo $wrapped_serial_no; ?></a>
-            </button>
-        </td>
-    </tr>
-<?php endforeach; ?>
-
+                <?php foreach ($recent_ecobricks as $ecobrick) : ?>
+                    <tr>
+                        <td>
+                            <img src="https://ecobricks.org/<?php echo htmlspecialchars($ecobrick['ecobrick_thumb_photo_url']); ?>"
+                                 alt="Ecobrick Thumbnail"
+                                 class="table-thumbnail"
+                                 onclick="ecobrickPreview('<?php echo htmlspecialchars($ecobrick['ecobrick_full_photo_url']); ?>', '<?php echo htmlspecialchars($ecobrick['serial_no']); ?>', '<?php echo htmlspecialchars($ecobrick['weight_g']); ?>g', '<?php echo htmlspecialchars($ecobrick['ecobricker_maker']); ?>', '<?php echo htmlspecialchars($ecobrick['location_full']); ?>')">
+                        </td>
+                        <td><?php echo htmlspecialchars($ecobrick['weight_g']); ?>g</td>
+                        <td><?php echo htmlspecialchars($ecobrick['location_full']); ?></td>
+                        <td><?php echo htmlspecialchars($ecobrick['status']); ?></td>
+                        <td>
+                            <button class="serial-button">
+                                <?php $serial_no = htmlspecialchars($ecobrick['serial_no']); $wrapped_serial_no = substr($serial_no, 0, 3) . '<br>' . substr($serial_no, 3, 3); ?>
+                                <a href="brik.php?serial_no=<?php echo $serial_no; ?>"><?php echo $wrapped_serial_no; ?></a>
+                            </button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
             </table>
         </div>
-
-
-</div><!--closes dashboard content-->
-
-
+    </div>
 </div>
 
 </div><!--closes main and starry background-->
 
 <!-- FOOTER STARTS HERE -->
-<?php require_once ("../footer-2024.php"); ?>
+<?php require_once("../footer-2024.php"); ?>
 
 <script type="text/javascript">
 function showModalInfo(type) {
@@ -194,6 +150,11 @@ document.getElementById('log-ecobrick-button').addEventListener('click', functio
     // Redirect to the log.php page
     window.location.href = 'log.php';
 });
+
+function loginUser() {
+    // Redirect to the login.php page
+    window.location.href = 'login.php';
+}
 </script>
 
 </body>
---
+++ includes/newest-briks-inc.php
@@ -0,0 +1,41 @@
+
+
+<?php require_once ("../meta/newest-briks-$lang.php");?>
+
+
+<STYLE>
+
+    #main {
+        height: fit-content !important;
+  padding-bottom: 100px;
+    }
+
+
+        .advanced-box-content {
+    padding: 2px 15px 15px 15px;
+    max-height: 0;  /* Initially set to 0 */
+    overflow: hidden;  /* Hide any overflowing content */
+    transition: max-height 0.5s ease-in-out;  /* Transition effect */
+	font-size:smaller;
+	margin-top:-10px;
+}
+
+
+.dropdown {
+  float: right;
+  overflow: hidden;
+  margin-bottom: -10px;
+}
+
+#splash-bar {
+  background-color: var(--top-header);
+  filter: none !important;
+  margin-bottom: -200px !important;
+}
+
+
+
+
+</style>
+
+<?php require_once ("../header-2024.php");?>

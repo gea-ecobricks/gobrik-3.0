0f5a7c06974f63d6a15d6b71b4ed44117f4917e3
Russell Maier <russ@earthen.io>
2024-08-13 22:41:22 +0200

    bottom

--- en/activate-2.php
+++ en/activate-2.php
@@ -8,6 +8,9 @@ ini_set('display_errors', 1);
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+require '../vendor/phpmailer/phpmailer/src/Exception.php';
+require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require '../vendor/phpmailer/phpmailer/src/SMTP.php';
 
 // PART 1: Setup
 // Initialize variables

d88d0025fe448657818cf9f10b8ef612b385349d
Russell Maier <russ@earthen.io>
2024-08-13 10:43:17 +0200

    bottom

--- en/activate.php
+++ en/activate.php
@@ -101,10 +101,10 @@ https://github.com/gea-ecobricks/gobrik-3.0/tree/main/en-->
 
         <p data-lang-id="005-links" style="font-size:1em; text-align: center;">Buwana accounts are designed with ecology, security, and privacy in mind. Soon, you'll be able to login to other great regenerative apps movement in the same way you login to GoBrik!.</p>
         <div style="display:flex;flex-flow:row;justify-content:center;width:100%;">
-            <div><a href="#" onclick="showModalInfo('terms')" class="underline-link" style="margin:auto;padding: 15px;">Our new Terms of Service</a>.</div>
+            <div><a href="#" onclick="showModalInfo('terms')" class="underline-link" style="margin:auto;padding: 15px;">Buwana & GoBrik Terms of Service</a>.</div>
 
-            <div><a href="https://earthen.io/gobrik-regen" class="underline-link" target="_blank" style="margin:auto;padding: 15px;">The Great GoBrik Regeneration</a></div>
-            <div><a href="https://github.com/gea-ecobricks/gobrik-3.0" class="underline-link" target="_blank" style="margin:auto;padding: 15px;">GoBrik 3.0 Code Repository</a></div>
+            <div><a href="https://earthen.io/gobrik-regen" class="underline-link" target="_blank" style="margin:auto;padding: 15px;">Blog post on: The Great GoBrik Regeneration</a></div>
+            <div><a href="https://github.com/gea-ecobricks/gobrik-3.0" class="underline-link" target="_blank" style="margin:auto;padding: 15px;">GoBrik 3.0 Open Source Code Repository</a></div>
        </div>
 
 <hr style="margin: 70px 10px 40px 10px;">

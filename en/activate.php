62c6ed1114d8db1d8450aa8eba8217d602906efb
Russell Maier <russ@earthen.io>
2024-08-14 10:02:10 +0200

    bottom

--- en/activate.php
+++ en/activate.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 $response = ['success' => false];
 $ecobricker_id = $_GET['user_id'] ?? null;
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.468';
+$version = '0.47';
 $page = 'activate';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 $first_name = '';
@@ -131,62 +131,6 @@ https://github.com/gea-ecobricks/gobrik-3.0/tree/main/en-->
 <!--FOOTER STARTS HERE-->
 <?php require_once ("../footer-2024.php"); ?>
 
-<script>
-
-/*SHOW MODALS*/
-
-function showModalInfo(type) {
-    const modal = document.getElementById('form-modal-message');
-    const photobox = document.getElementById('modal-photo-box');
-    const messageContainer = modal.querySelector('.modal-message');
-    const modalBox = document.getElementById('modal-content-box');
-    let content = '';
-    photobox.style.display = 'none';
-    switch (type) {
-        case 'terms':
-            content = `
-                <div style="font-size: small;">
-                    <?php include "../files/terms-$lang.php"; ?>
-                </div>
-            `;
-            modal.style.position = 'absolute';
-            modal.style.overflow = 'auto';
-            modalBox.style.textAlign = 'left';
-            modalBox.style.maxHeight = 'unset';
-            modalBox.style.marginTop = '30px';
-            modalBox.style.marginBottom = '30px';
-            modalBox.scrollTop = 0;
-            modal.style.alignItems = 'flex-start';
-
-            break;
-        case 'earthen':
-            content = `
-                <img src="../svgs/earthen-newsletter-logo.svg" alt="Earthen Newsletter" height="250px" width="250px" class="preview-image">
-                <div class="preview-title">Earthen Newsletter</div>
-                <div class="preview-text">We use our Earthen email newsletter to keep our users informed of the latest developments in the plastic transition movement and the world of ecobricking. Free with your GoBrik account or unclick to opt-out. We use ghost.org's open source newsletter platform that makes it easy to unsubscribe anytime.</div>
-            `;
-            break;
-        case 'ecobrick':
-            content = `
-                <img src="../webps/faqs-400px.webp" alt="Ecobrick Term and Types" height="200px" width="200px" class="preview-image">
-                <div class="preview-title">The Term</div>
-                <div class="preview-text">In 2016 plastic transition leaders around the world, agreed to use the non-hyphenated, non-capitalize term ‘ecobrick’ as the consistent, standardized term of reference in the guidebook and their materials. In this way, ecobrickers around the world would be able to refer with one word to same concept and web searches and hashtags would accelerate global dissemination.</div>
-            `;
-            break;
-        default:
-            content = '<p>Invalid term selected.</p>';
-    }
-
-    messageContainer.innerHTML = content;
-
-    // Show the modal and update other page elements
-    modal.style.display = 'flex';
-    document.getElementById('page-content').classList.add('blurred');
-    document.getElementById('footer-full').classList.add('blurred');
-    document.body.classList.add('modal-open');
-}
-
-</script>
 
 <script>
 function confirmDeletion() {

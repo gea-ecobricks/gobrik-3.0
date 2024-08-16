932b0c81d5648f40d9031a7216c6de0f9644b9bf
Russell Maier <russ@earthen.io>
2024-08-16 09:08:43 +0200

    bottom

--- en/confirm-email.php
+++ en/confirm-email.php
@@ -161,6 +161,7 @@ https://github.com/gea-ecobricks/gobrik-3.0/tree/main/en-->
 <!--FOOTER STARTS HERE-->
 <?php require_once ("../footer-2024.php"); ?>
 
+
 <script>
 document.addEventListener('DOMContentLoaded', function() {
     var code = "AYYEW";

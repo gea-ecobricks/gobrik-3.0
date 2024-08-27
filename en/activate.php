7d687a5ba68cd89467491bb96a35298bcea8b3a7
Russell Maier <russ@earthen.io>
2024-08-27 19:22:41 +0200

    bottom

--- en/activate.php
+++ en/activate.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 $response = ['success' => false];
 $ecobricker_id = $_GET['id'] ?? null;
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.472';
+$version = '0.475';
 $page = 'activate';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 $first_name = '';

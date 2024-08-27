704d926d99321a406c3295d98dadd6404251a8e2
Russell Maier <russ@earthen.io>
2024-08-27 20:54:53 +0200

    bottom

--- en/newest-briks.php
+++ en/newest-briks.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 
 // Set up page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.42';
+$version = '0.43';
 $page = 'newest-briks';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 

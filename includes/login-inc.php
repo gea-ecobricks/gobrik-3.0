9d2d399b3769282b108a020cf7b28d10768b54ad
Russell Maier <russ@earthen.io>
2024-08-29 16:32:30 +0200

    bottom

--- includes/login-inc.php
+++ includes/login-inc.php
@@ -143,6 +143,7 @@
                         inset 0 -2px 5px rgba(0, 0, 0, 0.3);
         }
 
+
         #password:checked ~ .slider {
             left: 0;
         }
@@ -230,13 +231,7 @@
                 inset 0 -2px 5px rgba(0, 0, 0, 0.3);
 }
 
-#password:checked ~ .slider {
-    left: 0;
-}
 
-#code:checked ~ .slider {
-    left: 20%;
-}
 
 
 

89bc14668d0449c19342fc3a7809d027f5b70ec9
Russell Maier <russ@earthen.io>
2024-08-29 04:48:12 +0200

    bottom

--- en/login.php
+++ en/login.php
@@ -149,10 +149,10 @@ echo '</script>';
             <input type="radio" id="password" name="toggle" checked>
             <input type="radio" id="code" name="toggle">
             <div class="toggle-button password">🔑</div>
-            <div class="toggle-button code">🕵📲</div>
+            <div class="toggle-button code">📱</div>
             <div class="slider"></div>
             <input type="submit" id="submit-password-button" value="Login" class="login-button-75">
-            <input type="submit" id="send-code-button" value="📨 Send Code" class="code-button-75" style="display:none;">
+            <input type="submit" id="send-code-button" value="📨 Send Code" class="code-button-75">
         </div>
     </div>
 </form>

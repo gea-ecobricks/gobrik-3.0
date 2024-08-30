c1207f272ae238cfccdcc9107ee46abe44aca751
Russell Maier <russ@earthen.io>
2024-08-30 22:58:26 +0200

    bottom

--- en/confirm_code.php
+++ en/confirm_code.php
@@ -1,38 +1,13 @@
 <?php
-session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-
-//PART 1 Activation
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php';  // Path to PHPMailer
+require '../vendor/autoload.php'; // Path to PHPMailer
 
-$response = array();
-$credential_key = $_POST['credential_key'] ?? '';
-$lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-
-if (empty($credential_key)) {
-    $response['status'] = 'empty_fields';
-    echo json_encode($response);
-    exit();
-}
-
-require_once("../gobrikconn_env.php");
-
-
-//PART 2 FUNCTIONS
-
-function generateCode() {
-    return strtoupper(substr(bin2hex(random_bytes(3)), 0, 5));
-}
-
-function sendVerificationCode($email_addr, $login_code, $buwana_id) {
+function sendVerificationCode($first_name, $email_addr, $verification_code) {
     $mail = new PHPMailer(true);
     try {
+        // Server settings
         $mail->isSMTP();
         $mail->Host = 'mail.ecobricks.org';
         $mail->SMTPAuth = true;
@@ -41,94 +16,19 @@ function sendVerificationCode($email_addr, $login_code, $buwana_id) {
         $mail->SMTPSecure = false;
         $mail->Port = 26;
 
+        // Recipients
         $mail->setFrom('gobrik@ecobricks.org', 'GoBrik Team');
         $mail->addAddress($email_addr);
 
+        // Content
         $mail->isHTML(true);
-        $mail->Subject = 'GoBrik Login Code';
-        $mail->Body = "Hello,<br><br>Your code to login to your account is: <b>$login_code</b><br><br>Return to your browser and enter the code or click this link to login directly:<br><br>https://beta.gobrik.com/login.php?code=$buwana_id+$login_code<br><br>The GoBrik team";
+        $mail->Subject = 'GoBrik Verification Code';
+        $mail->Body = "Hello $first_name!<br><br>If you're reading this, we're glad! The code to activate your account is:<br><br><b>$verification_code</b><br><br>Return back to your browser and enter the code.<br><br>The GoBrik team";
 
         $mail->send();
         return true;
     } catch (Exception $e) {
-        file_put_contents('debug.log', "Mailer Error: " . $e->getMessage() . "\n", FILE_APPEND);  // Log detailed error
         return false;
     }
 }
-
-// PART 3 Check GoBrik to see if user account is activated
-$sql_check_email = "SELECT ecobricker_id, buwana_activated, email_addr, first_name FROM tb_ecobrickers WHERE email_addr = ?";
-$stmt_check_email = $gobrik_conn->prepare($sql_check_email);
-if ($stmt_check_email) {
-    $stmt_check_email->bind_param('s', $credential_key);
-    $stmt_check_email->execute();
-    $stmt_check_email->store_result();
-
-    if ($stmt_check_email->num_rows === 1) {
-        $stmt_check_email->bind_result($ecobricker_id, $buwana_activated, $email_addr, $first_name);
-        $stmt_check_email->fetch();
-
-        if ($buwana_activated == '0') {
-            $response['status'] = 'activation_required';
-            $response['redirect'] = "activate.php?id=$ecobricker_id";
-            echo json_encode($response);
-            exit();
-        }
-        $stmt_check_email->close();
-    } else {
-        $stmt_check_email->close();
-        $response['status'] = 'not_found';
-        echo json_encode($response);
-        exit();
-    }
-} else {
-    $response['status'] = 'error';
-    echo json_encode($response);
-    exit();
-}
-
-// PART 4: Check Buwana Database for the credential
-// PART 4: Check Buwana Database for the credential
-require_once("../buwanaconn_env.php");
-
-// Prepare the SQL statement to check for the existence of the credential_key
-$sql_credential = "SELECT buwana_id FROM credentials_tb WHERE credential_key = ?";
-$stmt_credential = $buwana_conn->prepare($sql_credential);
-if ($stmt_credential) {
-    $stmt_credential->bind_param('s', $credential_key);
-    $stmt_credential->execute();
-    $stmt_credential->store_result();
-
-    // Check if the credential_key exists in the database
-    if ($stmt_credential->num_rows === 1) {
-        $stmt_credential->bind_result($buwana_id);
-        $stmt_credential->fetch();
-        $stmt_credential->close();
-
-        // Credential found, respond with credfound status
-        $response['status'] = 'credfound';
-        $response['buwana_id'] = $buwana_id; // Optionally return the buwana_id
-        echo json_encode($response);
-        exit();
-    } else {
-        // No credential found with the given key
-        $stmt_credential->close();
-        $response['status'] = 'crednotfound';
-        echo json_encode($response);
-        exit();
-    }
-} else {
-    // SQL preparation failed, log the error and respond
-    file_put_contents('debug.log', "SQL Prep Error: " . $buwana_conn->error . "\n", FILE_APPEND);
-    $response['status'] = 'error';
-    $response['message'] = 'Error preparing statement for credentials_tb: ' . $buwana_conn->error;
-    echo json_encode($response);
-    exit();
-}
-
-// Close the database connections
-$buwana_conn->close();
-$gobrik_conn->close();
-
-
 ?>

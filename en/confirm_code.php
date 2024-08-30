2ebfbe517637fef148e3010fd348e7aa2d99939f
Russell Maier <russ@earthen.io>
2024-08-30 22:04:57 +0200

    bottom

--- en/confirm_code.php
+++ en/confirm_code.php
@@ -1,19 +1,16 @@
 <?php
+
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
-//PART 1 Activation
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php';  // Path to PHPMailer
+require '../vendor/autoload.php'; // Path to PHPMailer
 
 $response = array();
 $credential_key = $_POST['credential_key'] ?? '';
-$lang = basename(dirname($_SERVER['SCRIPT_NAME']));
 
 if (empty($credential_key)) {
     $response['status'] = 'empty_fields';
@@ -21,43 +18,9 @@ if (empty($credential_key)) {
     exit();
 }
 
-require_once("../gobrikconn_env.php");
+require_once ("../gobrikconn_env.php");
 
-
-//PART 2 FUNCTIONS
-
-function generateCode() {
-    return strtoupper(substr(bin2hex(random_bytes(3)), 0, 5));
-}
-
-function sendVerificationCode($email_addr, $login_code, $buwana_id) {
-    $mail = new PHPMailer(true);
-    try {
-        $mail->isSMTP();
-        $mail->Host = 'mail.ecobricks.org';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'gobrik@ecobricks.org';
-        $mail->Password = '1Welcome!';
-        $mail->SMTPSecure = false;
-        $mail->Port = 26;
-
-        $mail->setFrom('gobrik@ecobricks.org', 'GoBrik Team');
-        $mail->addAddress($email_addr);
-
-        $mail->isHTML(true);
-        $mail->Subject = 'GoBrik Login Code';
-        $mail->Body = "Hello,<br><br>Your code to login to your account is: <b>$login_code</b><br><br>Return to your browser and enter the code or click this link to login directly:<br><br>https://beta.gobrik.com/login.php?code=$buwana_id+$login_code<br><br>The GoBrik team";
-
-        $mail->send();
-        return true;
-    } catch (Exception $e) {
-        file_put_contents('debug.log', "Mailer Error: " . $e->getMessage() . "\n", FILE_APPEND);  // Log detailed error
-        return false;
-    }
-}
-
-// PART 3 Check GoBrik to see if user account is activated
-$sql_check_email = "SELECT ecobricker_id, buwana_activated, email_addr, first_name FROM tb_ecobrickers WHERE email_addr = ?";
+$sql_check_email = "SELECT ecobricker_id, buwana_activated FROM tb_ecobrickers WHERE email_addr = ?";
 $stmt_check_email = $gobrik_conn->prepare($sql_check_email);
 if ($stmt_check_email) {
     $stmt_check_email->bind_param('s', $credential_key);
@@ -65,7 +28,7 @@ if ($stmt_check_email) {
     $stmt_check_email->store_result();
 
     if ($stmt_check_email->num_rows === 1) {
-        $stmt_check_email->bind_result($ecobricker_id, $buwana_activated, $email_addr, $first_name);
+        $stmt_check_email->bind_result($ecobricker_id, $buwana_activated);
         $stmt_check_email->fetch();
 
         if ($buwana_activated == '0') {
@@ -74,6 +37,7 @@ if ($stmt_check_email) {
             echo json_encode($response);
             exit();
         }
+
         $stmt_check_email->close();
     } else {
         $stmt_check_email->close();
@@ -87,12 +51,10 @@ if ($stmt_check_email) {
     exit();
 }
 
-// PART 4: Consult Buwana Credential Table
-
 require_once ("../buwanaconn_env.php");
 
 $sql_credential = "SELECT buwana_id, 2fa_issued_count FROM credentials_tb WHERE credential_key = ?";
-$stmt_credential = $buwana_conn->prepare($sql_credential);
+$stmt_credential = $gobrik_conn->prepare($sql_credential);
 if ($stmt_credential) {
     $stmt_credential->bind_param('s', $credential_key);
     $stmt_credential->execute();
@@ -103,12 +65,11 @@ if ($stmt_credential) {
         $stmt_credential->fetch();
         $stmt_credential->close();
 
-        // Generate a new 2FA temporary code
-        $temp_code = substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 6);
+        // Corrected to generate a 5-character alphanumeric code
+        $temp_code = substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 5);
         $issued_datetime = date('Y-m-d H:i:s');
         $new_issued_count = $issued_count + 1;
 
-        // Update the credentials_tb with new 2FA details
         $sql_update = "UPDATE credentials_tb SET
                        2fa_temp_code = ?,
                        2fa_code_issued = ?,
@@ -122,29 +83,25 @@ if ($stmt_credential) {
 
             $response['status'] = 'credfound';
             $response['buwana_id'] = $buwana_id;
-            $response['2fa_code'] = $temp_code; // Optionally return the code in the response
+            $response['2fa_code'] = $temp_code;
             echo json_encode($response);
             exit();
         } else {
             $response['status'] = 'error';
-            $response['message'] = 'Failed to update 2FA details: ' . $buwana_conn->error;
             echo json_encode($response);
             exit();
         }
     } else {
         $response['status'] = 'crednotfound';
-        $response['message'] = 'Credential not found';
         echo json_encode($response);
         exit();
     }
 } else {
     $response['status'] = 'error';
-    $response['message'] = 'Error preparing statement for credentials_tb: ' . $buwana_conn->error;
     echo json_encode($response);
     exit();
 }
 
-// Close the database connections
 $buwana_conn->close();
 $gobrik_conn->close();
 

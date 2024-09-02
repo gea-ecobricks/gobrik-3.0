ec9971cf5ee3ba6044d03efb5213e788bf2bd93b
Russell Maier <russ@earthen.io>
2024-09-02 22:13:40 +0200

    code entry

--- en/profile.php
+++ en/profile.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 1);
 // Set up page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
 $version = '0.37';
-$page = 'profile';
+$page = 'dashboard';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 
 // Check if the user is logged in

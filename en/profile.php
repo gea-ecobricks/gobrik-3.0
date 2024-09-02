07f1bcfa28fc1cee30f0e54dd1253be075d938c6
Russell Maier <russ@earthen.io>
2024-09-02 22:40:14 +0200

    code entry

--- en/profile.php
+++ en/profile.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 
 // Set up page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.37';
+$version = '0.38';
 $page = 'profile';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 

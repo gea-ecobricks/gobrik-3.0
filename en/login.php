e685eb9b3725ee6108aee15209d50be89b3e43ff
Russell Maier <russ@earthen.io>
2024-09-03 14:51:07 -0400

    code entry

--- en/login.php
+++ en/login.php
@@ -19,7 +19,7 @@ if (empty($_SESSION['csrf_token'])) {
 
 // VERSION Set page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.73';
+$version = '0.74';
 $page = 'login';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 
--- en/profile.php
+++ en/profile.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 
 // Set up page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.381';
+$version = '0.382';
 $page = 'profile';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 
@@ -18,7 +18,7 @@ if (!isset($_SESSION['buwana_id'])) {
     </script>';
     exit();
 }
-?>
+
 
 $buwana_id = $_SESSION['buwana_id'];
 

eefd5d7747dbf36cd5342a0fad751c428480000a
Russell Maier <russ@earthen.io>
2024-09-05 14:43:55 -0400

    code entry

--- en/login.php
+++ en/login.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 
 // Set page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.751';
+$version = '0.76';
 $page = 'login';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 

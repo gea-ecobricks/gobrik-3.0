67f7c8871b37036b39f567303849502597ad0402
Russell Maier <russ@earthen.io>
2024-09-09 10:04:51 -0400

    code entry

--- en/profile.php
+++ en/profile.php
@@ -349,7 +349,7 @@ echo '<!DOCTYPE html>
     <!--earthen db check-->
 <div class="form-container" style="padding-top:20px">
     <h2>Earthen Newsletter Subscription Status</h2>
-    <p>Check to see if your <?php echo htmlspecialchars($email_addr); ?> is subscribed to the Earthen newsletter</p>
+    <p>Check to see if your <?php echo htmlspecialchars($email); ?> is subscribed to the Earthen newsletter</p>
     <p id="earthen-status-message" style="display:none;"></p>
     <button id="check-earthen-status">Check Earthen Status</button>
 </div>

2b85ed0a0bab760c858396d4ee6897ed5c7933e3
Russell Maier <russ@earthen.io>
2024-09-11 10:17:10 -0400

    code entry

--- en/dashboard.php
+++ en/dashboard.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 
 // Set up page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.376';
+$version = '0.377';
 $page = 'dashboard';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 

d9b34d83e188dd50a99b840933fa55af2fc08b6a
Russell Maier <russ@earthen.io>
2024-09-16 10:57:17 -0400

    code entry

--- en/brik.php
+++ en/brik.php
@@ -8,7 +8,7 @@ ini_set('display_errors', 1);
 
 // Set page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.764';
+$version = '0.765';
 $page = 'brik';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 

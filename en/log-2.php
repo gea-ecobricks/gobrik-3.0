c63f9871127b1a6fd6fdfd692952595d1e5b8923
Russell Maier <russ@earthen.io>
2024-09-17 21:14:27 -0400

    code entry

--- en/log-2.php
+++ en/log-2.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 
 // Set up page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.491';
+$version = '0.492';
 $page = 'log-2';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 

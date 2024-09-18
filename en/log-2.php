894ad3c99e5c1c093e6fc40c8c35ca03c744f736
Russell Maier <russ@earthen.io>
2024-09-18 11:17:05 -0400

    code entry

--- en/log-2.php
+++ en/log-2.php
@@ -407,7 +407,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     try {
                         var response = JSON.parse(xhr.responseText);
                         var ecobrick_unique_id = response.ecobrick_unique_id;
-                        window.location.href = 'log-3php?id=' + ecobrick_unique_id; // Redirect to success page with ecobrick_unique_id
+                        window.location.href = 'log-3.php?id=' + ecobrick_unique_id; // Redirect to success page with ecobrick_unique_id
                     } catch (e) {
                         console.error('Error parsing server response:', e);
                         handleFormResponse(xhr.responseText); // Handle error response

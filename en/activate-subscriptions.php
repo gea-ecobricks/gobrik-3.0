e6df8a6af99e55f3ffc74cc3c1f0f9fb86454a5c
Russell Maier <russ@earthen.io>
2024-09-26 22:37:38 -0400

    Russ changes to activate sequence

--- en/activate-subscriptions.php
+++ en/activate-subscriptions.php
@@ -22,7 +22,7 @@ $is_logged_in = false; // Ensure not logged in for this page
 // Set page variables
 $page = 'activate-subscriptions';
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.776';
+$version = '0.777';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 $response = ['success' => false];
 $buwana_id = $_GET['id'] ?? null;
--- includes/activate-subscriptions-inc.php
+++ includes/activate-subscriptions-inc.php
@@ -1,135 +1,30 @@
 
 
-<?php require_once ("../meta/$page-$lang.php");?>
+<?php require_once ("../meta/activate-subscriptions-en.php");?>
 
 
 <STYLE>
 
-/* Container for subscription boxes */
-.subscription-boxes {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 20px;
-}
-
-/* Individual subscription box */
-.sub-box {
-    display: flex;
-    position: relative;
-    border: 1px solid rgba(128, 128, 128, 0.5);
-    border-radius: 10px;
-    padding: 15px;
-    align-items: flex-start;
-    transition: border 0.5s;
-    cursor: pointer;
-    width: calc(50% - 20px); /* Two columns when screen width is above 1000px */
-    box-sizing: border-box;
-}
-
-/* Checkbox for selection */
-.sub-checkbox {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    opacity: 0;
-    cursor: pointer;
-}
-
-/* Label for the checkbox to display a custom style */
-.checkbox-label {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    width: 20px;
-    height: 20px;
-    border: 1px solid grey;
-    border-radius: 4px;
-    background-color: white;
-    cursor: pointer;
-}
-
-/* Style when the checkbox is checked */
-.sub-checkbox:checked + .checkbox-label {
-    background-color: green;
-    border-color: green;
-}
-
-/* Icon inside the box */
-.sub-icon {
-    width: 70px;
-    height: 70px;
-    background-color: grey;
-    border-radius: 4px;
-    margin-right: 15px;
-    align-self: flex-start;
-}
-
-/* Content area of the box */
-.sub-content {
-    text-align: left;
-    flex: 1;
-}
-
-/* Style for titles, authors, descriptions, and languages */
-.sub-name {
-    margin: 0;
-}
-
-.sub-sender-name {
-    font-family: 'Mulish', sans-serif;
-    font-size: 12px;
-    color: grey;
-    margin: 5px 0;
-    text-align: left;
-}
-
-.sub-description {
-    margin: 5px 0;
-}
-
-.subscription-language {
-    font-family: 'Mulish', sans-serif;
-    font-size: 12px;
-    color: grey;
-    margin: 5px 0;
-    text-align: left;
-}
-
-/* Hover effect changes border color */
-.sub-box:hover {
-    border: 2px solid green;
-}
-
-/* When box is selected, change border to strong color */
-.sub-box.selected {
-    border: 2px solid green;
-    border-color: rgba(0, 128, 0, 1);
-}
-
-/* Responsive behavior: single column for screen widths below 1000px */
-@media (max-width: 1000px) {
-    .sub-box {
-        width: 100%;
+       #main {
+        height: fit-content !important;
+  padding-bottom: 100px;
     }
-}
 
-/* Submit button at the bottom of the form */
-.submit-button {
-    display: block;
-    margin: 20px auto;
-    padding: 10px 20px;
-    background-color: green;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    font-size: 16px;
-    cursor: pointer;
-    transition: background-color 0.3s;
-}
 
-.submit-button:hover {
-    background-color: darkgreen;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

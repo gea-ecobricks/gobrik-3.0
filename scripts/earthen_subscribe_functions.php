089d93034725c4f1b72d560c2093f259072492ec
Russell Maier <russ@earthen.io>
2024-09-26 22:33:28 -0400

    Russ changes to activate sequence

--- scripts/earthen_subscribe_functions.php
+++ scripts/earthen_subscribe_functions.php
@@ -98,29 +98,29 @@ function grabActiveEarthenSubs() {
                 foreach ($response_data['newsletters'] as $newsletter) {
                     if ($newsletter['status'] === 'active') {
                        // Extract data
-                        $id = htmlspecialchars($newsletter['id']);
-                        $name = htmlspecialchars($newsletter['name']);
-                        $description = htmlspecialchars($newsletter['description']);
-                        $sender_name = htmlspecialchars($newsletter['sender_name']);
-                        $language = "English"; // Adjust if data in the JSON specifies a different language
-                        $frequency = "1-3 posts a month"; // Hard-coded frequency for demonstration
+                        $sub_id = htmlspecialchars($newsletter['id']);
+                        $sub_name = htmlspecialchars($newsletter['name']);
+                        $sub_description = htmlspecialchars($newsletter['description']);
+                        $sub_sender_name = htmlspecialchars($newsletter['sender_name']);
+                        $sub_language = "English"; // Adjust if data in the JSON specifies a different language
+                        $sub_frequency = "1-3 posts a month"; // Hard-coded frequency for demonstration
 
                         // Output the subscription box HTML
                         echo "
-                            <div id=\"{$id}\" class=\"sub-box\" data-color=\"green\">
-                                <input type=\"checkbox\" class=\"sub-checkbox\" id=\"checkbox-{$id}\" name=\"subscriptions[]\" value=\"{$id}\">
-                                <label for=\"checkbox-{$id}\" class=\"checkbox-label\"></label>
-                                <div class=\"sub-image\" style=\"background-image: url('path-to-icons/{$id}-icon.webp');\"></div>
+                            <div id=\"{$sub_id}\" class=\"sub-box\" data-color=\"green\">
+                                <input type=\"checkbox\" class=\"sub-checkbox\" id=\"checkbox-{$sub_id}\" name=\"subscriptions[]\" value=\"{$sub_id}\">
+                                <label for=\"checkbox-{$sub_id}\" class=\"checkbox-label\"></label>
+                                <div class=\"sub-image\"></div>
                                 <div class=\"sub-content\">
                                     <div class=\"sub-header\">
-                                        <div class=\"sub-icon\" style=\"background-image: url('path-to-icons/{$id}-icon.webp');\"></div>
+                                        <div class=\"sub-icon\"></div>
                                         <div class=\"sub-header-text\">
-                                            <div class=\"sub-name\">{$name}</div>
-                                            <div class=\"sub-sender-name\">by {$sender_name}</div>
+                                            <div class=\"sub-name\">{$sub_name}</div>
+                                            <div class=\"sub-sender-name\">by {$sub_sender_name}</div>
                                         </div>
                                     </div>
-                                    <div class=\"sub-description\">{$description}</div>
-                                    <div class=\"sub-lang\">{$language} | {$frequency}</div>
+                                    <div class=\"sub-description\">{$sub_description}</div>
+                                    <div class=\"sub-lang\">{$sub_language} | {$sub_frequency}</div>
                                 </div>
                             </div>
                         ";

c82d70b4c95246099034520ac2bfc2fd190a697e
Russell Maier <russ@earthen.io>
2024-09-28 15:09:09 -0400

    Russ changes to activate sequence

--- scripts/earthen_subscribe_functions.php
+++ scripts/earthen_subscribe_functions.php
@@ -37,7 +37,7 @@ function createGhostJWT() {
     $payload = json_encode([
         'iat' => $now,
         'exp' => $now + 300, // Token valid for 5 minutes
-        'aud' => 'admin/' // Audience value
+        'aud' => '4.0' // Audience value
     ]);
 
     // Encode Header and Payload

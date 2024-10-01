040f96b50aa241c785668e45463e6e69d0856eaa
Russell Maier <russ@earthen.io>
2024-10-01 00:07:51 -0400

    Russ changes to activate sequence

--- en/profile.php
+++ en/profile.php
@@ -35,7 +35,7 @@ if ($is_logged_in) {
     $user_location_watershed = getWatershedName($buwana_conn, $buwana_id);
     $first_name = getUserFirstName($buwana_conn, $buwana_id);
     $gea_status = getGEA_status($buwana_id);
-    $community_name = getCommunityName($buwana_id);
+    $community_name = getCommunityName($buwana_conn, $buwana_id);
 
     // Fetch user information including community_id, location_watershed, and location_full
     $sql_user_info = "SELECT full_name, first_name, last_name, email, country_id, languages_id, birth_date,

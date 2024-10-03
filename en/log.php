1dab7205377e51d8c921b3b57c97608e788e321c
Russell Maier <russ@earthen.io>
2024-10-02 20:42:28 -0400

    Russ changes to activate sequence

--- en/log.php
+++ en/log.php
@@ -385,8 +385,9 @@ require_once ("../includes/log-inc.php");
                                    value="<?= htmlspecialchars($user_community_name, ENT_QUOTES); ?>"
                                    placeholder="Start typing your community..." required style="padding-left:45px;">
                             <div id="community-pin" class="pin-icon">📌</div>
-                            <div id="community-suggestions" class="suggestions-box"></div>
                         </div>
+                        <div id="community-suggestions" class="suggestions-box"></div>
+
 
                         <p class="form-caption">Select your community from the list based on your current or updated community name.</p>
                     </div>

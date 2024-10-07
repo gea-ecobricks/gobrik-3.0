acd9f3912049ef4d7561cd4ef4540d22a2899154
Russell Maier <russ@earthen.io>
2024-10-07 12:35:57 -0400

    Russ changes to activate sequence

--- en/activate-3.php
+++ en/activate-3.php
@@ -3,7 +3,7 @@ require_once '../earthenAuth_helper.php'; // Include the authentication helper f
 
 // Set up page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.39';
+$version = '0.4';
 $page = 'signup';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 

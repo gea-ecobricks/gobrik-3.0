cc7b72b9cdb224b97e8bba3b6fc5eaeeef1dbada
Russell Maier <russ@earthen.io>
2024-10-08 10:53:02 -0400

    Russ changes to activate sequence

--- includes/profile-inc.php
+++ includes/profile-inc.php
@@ -12,9 +12,9 @@
 
 
 <!-- jQuery and jQuery UI (required for autocomplete) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 
 <STYLE>
 

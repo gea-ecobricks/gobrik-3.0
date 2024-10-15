89714cd42be2b08a8aff6959ecd20889c8432f93
Russell Maier <russ@earthen.io>
2024-10-15 19:32:19 -0400

    Russ changes to activate sequence

--- en/dashboard.php
+++ en/dashboard.php
@@ -3,7 +3,7 @@ require_once '../earthenAuth_helper.php'; // Include the authentication helper f
 
 // Set up page variables
 $lang = basename(dirname($_SERVER['SCRIPT_NAME']));
-$version = '0.42';
+$version = '0.43';
 $page = 'dashboard';
 $lastModified = date("Y-m-d\TH:i:s\Z", filemtime(__FILE__));
 

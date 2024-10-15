1b43208a3a9ead76b46043d860d97869f61a3b4b
Russell Maier <russ@earthen.io>
2024-10-15 16:59:47 -0400

    Russ changes to activate sequence

--- en/dashboard.php
+++ en/dashboard.php
@@ -165,9 +165,11 @@ https://github.com/gea-ecobricks/gobrik-3.0/tree/main/en-->
                             <td><?php echo htmlspecialchars($ecobrick['status']); ?></td>
                             <td>
                                 <button class="serial-button" onclick="viewEcobrickActions('<?php echo $serial_no; ?>', '<?php echo $ecobrick['status']; ?>', '<?php echo $lang; ?>')">
-                                    <?php $wrapped_serial_no = substr($serial_no, 0, 3) . '<br>' . substr($serial_no, 3, 3); ?>
-                                    <?php echo $wrapped_serial_no; ?>
-                                </button>
+    <?php
+    $wrapped_serial_no = substr($serial_no, 0, 3) . '<br>' . substr($serial_no, 3, 3);
+    echo $wrapped_serial_no;
+    ?>
+</button>
 
 
                             </td>

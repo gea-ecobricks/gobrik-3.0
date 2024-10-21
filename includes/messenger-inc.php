13f7aa5b5664eb173c4783eb78327ce7b6315a59
Russell Maier <russ@earthen.io>
2024-10-21 13:29:50 -0400

    Update fetch

--- includes/messenger-inc.php
+++ includes/messenger-inc.php
@@ -5,7 +5,6 @@
 
 
 <!-- Include DataTables and jQuery Libraries -->
-<link rel="stylesheet" type="text/css" href="../styles/messenger.css">
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css">
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.dataTables.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>

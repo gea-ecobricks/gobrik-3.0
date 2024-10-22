4a81cc0a0453ea1559fc634c379382410878330b
Russell Maier <russ@earthen.io>
2024-10-22 18:18:06 -0400

    Update fetch

--- includes/admin-review-inc.php
+++ includes/admin-review-inc.php
@@ -49,126 +49,44 @@
         height: fit-content;
     }
 
-    .preview-text {
-        font-family: 'Mulish', Arial, Helvetica, sans-serif;
-        font-weight: 300;
-        -webkit-font-smoothing: antialiased;
-        color: var(--text-color);
-        margin-top: 15px;
-        margin-bottom: 15px;
-    }
 
-
-
-    @media screen and (min-width: 700px) {
-        .preview-text {
-            font-size: 1em;
-        }
-
-    }
-
-    @media screen and (max-width: 700px) {
-        .preview-text {
-            font-size: 0.8em;
-        }
-
-
-        #language-code {
-            display:none;}
-        }
-    }
-
-
-
-
-
-#main-background {
-  background-size: cover;
-
-}
-
-
-/* Media Query for screens under 700px */
-@media screen and (max-width: 700px) {
-  .form-container {
-    width: calc(100% - 40px);
-    margin: 0;
-    /* border: none; */
-    padding: 20px 20px 0 20px;
-    max-width: 600px;
-    padding: 20px;
-    position: relative;
-    margin-top: 80px;
-
-  }
-}
-
-#featured_image {
-  margin-bottom: 8px;
-  margin-top: 8px;
-  padding: 5px;
-  font-size: 1em;
-}
-
-#tmb_featured_image {
-  margin-bottom: 8px;
-  margin-top: 8px;
-  padding: 5px;
-  font-size: 1em;
-}
-
-/* Centering the form vertically on larger screens */
-@media screen and (min-width: 701px) {
-  /* #form-submission-box {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-  } */
-
-  .form-container {
-    margin-top: auto;
-    margin-bottom: auto;
-    padding: 30px;
-    margin-top: 110px;
-
-  }
-}
-
-.module-btn {
-  background: var(--emblem-green);
-  width: 100%;
-  display: flex;
-}
-
-.module-btn:hover {
-  background: var(--emblem-green-over);
-}
-
-
-.go-button {
+.ecobrick-action-button {
+    width: 100%;           /* Ensures the button takes the full width */
+    display: block;        /* Ensures the button behaves as a block element */
+    text-align: center;    /* Centers the text inside the button */
+    padding: 10px;         /* Add some padding for better button appearance */
+    margin-bottom: 10px;   /* Margin to create space between buttons */
+    border: none;          /* Remove default borders */
+    cursor: pointer;       /* Show pointer on hover */
+    font-size: 1em;        /* Consistent font size */
     padding: 10px 20px;
-    border: none;
-    color: white;
-    transition: background-color 0.3s, cursor 0.3s;
-     padding: 10px 20px;
   border: none;
-  border-radius: 6px;
-   font-size: 1.3em;
-
-    background-color: var(--button-2-1);
-    cursor: pointer;
-    margin: 10px;
+  border-radius: 10px;
+  cursor: pointer;
+  background: #00800094;
+  font-size: 1.3em;
+  text-align: center;
+  text-decoration: none;
+  color:white;
 }
 
-/* Hover effect for enabled state */
-.go-button:hover {
-    background-color: var(--button-2-1-over);
+
+.ecobrick-action-button:hover {
+  background: var(--emblem-green);
 }
 
-.underline-link {
-color: var(--text-color);
+
+/* Special styling for delete button */
+.ecobrick-action-button.deleter-button {
+    background-color: #ff000094; /* Red background for the delete button */
+    color: white;          /* White text for contrast */
 }
+
+.ecobrick-action-button.deleter-button:hover {
+    background-color: red; /* Red background for the delete button */
+    color: white;          /* White text for contrast */
+}
+
 
 
 #splash-bar {

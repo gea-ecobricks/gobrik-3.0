5c0dbdc07d2a8dedbba27b3187909a1be9caff88
Russell Maier <russ@earthen.io>
2024-10-23 01:13:33 -0400

    Update fetch

--- en/messenger.php
+++ en/messenger.php
@@ -502,9 +502,11 @@ $(document).ready(function() {
             document.getElementById("settings-buttons").style.padding = "12px 43px 12px 12px";
             document.getElementById("language-menu-slider").style.top = "-35px";
             document.getElementById("login-menu-slider").style.top = "-35px";
+            document.getElementById("form-submission-box").style.marginTop = "45px";
+
 
             // Adjust the padding-top of form-container
-            $('.form-container').animate({ 'padding-top': '30px' }, 1000); // Adjusts padding-top over 1 second
+            $('.form-container').animate({ 'padding-top': '20px' }, 1000); // Adjusts padding-top over 1 second
         }, 3000); // 3000 milliseconds = 3 seconds delay
     }
 

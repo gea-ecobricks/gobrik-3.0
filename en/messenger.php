545d09152eb7487e312a482ca23ec4f8268ecc44
Russell Maier <russ@earthen.io>
2024-10-23 01:03:59 -0400

    Update fetch

--- en/messenger.php
+++ en/messenger.php
@@ -500,6 +500,27 @@ $(document).ready(function() {
         }, 3000); // 3000 milliseconds = 3 seconds delay
     }
 
+
+function scrollMoreThan30() {
+    if (window.pageYOffset > 30 && window.pageYOffset < 800) {
+        document.getElementById("header").style.height = "60px";
+        document.getElementById("header").style.borderBottom = "var(--header-accent) 0.5px solid";
+        document.getElementById("header").style.boxShadow = "0px 0px 15px rgba(0, 0, 10, 0.805)";
+        document.getElementById("gea-logo").style.width = "170px";
+        document.getElementById("gea-logo").style.height = "35px";
+        document.getElementById("logo-gobrik").style.opacity = "0.9";
+        document.getElementById("settings-buttons").style.padding = "12px 43px 12px 12px";
+        document.getElementById("language-menu-slider").style.top = "-35px";
+        document.getElementById("login-menu-slider").style.top = "-35px";
+
+        // Tuck the top banner image under the header
+        var topPageImage = document.querySelector('.top-page-image');
+        if (topPageImage) {
+            topPageImage.style.zIndex = "25";
+        }
+    }
+}
+
     // Call the function when the document is ready
     adjustPageAfterSplash();
 });

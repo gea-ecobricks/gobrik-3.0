f49ab55ee9ccf96f8a1461777a8dcd5edf9b20d4
Russell Maier <russ@earthen.io>
2024-10-23 22:13:13 -0400

    Update fetch

--- en/messenger.php
+++ en/messenger.php
@@ -537,7 +537,7 @@ $(document).ready(function() {
             document.getElementById("language-menu-slider").style.top = "-35px";
             document.getElementById("login-menu-slider").style.top = "-35px";
             document.getElementById("form-submission-box").style.marginTop = "60px";
-            document.html.classList.add('modal-open');
+            document.page-content.classList.add('modal-open');
     }
 
 
